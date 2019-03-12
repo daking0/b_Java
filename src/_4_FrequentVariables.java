36a46deae1fbf9e003a6148cdaa22e57c36b7bb0
BITSC-5-22 <BITSC-5-22@BITSC-5-22-PC>
2019-03-12 20:01:37 +0900

    first1

---
+++ src/_4_FrequentVariables.java
@@ -0,0 +1,19 @@
+
+public class _4_FrequentVariables {
+
+	public static void main(String[] args) {
+		      int num = 100;
+		      double dnum = 3.14;
+		      char ch = '°¡';
+		      boolean isLogic = true;
+		      String name = "È«±æµ¿";
+		      
+		      System.out.println(num);
+		      System.out.println(dnum);
+		      System.out.println(ch);
+		      System.out.println(isLogic);
+		      System.out.println(name);
+
+	}
+
+}
